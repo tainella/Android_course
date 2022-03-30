e001947c36f19073689407960952e27faacba733
tainella <priklucheniye@mail.ru>
2022-03-30 22:27:54 +0300

    b

--- my_project/block-ui/app/src/main/java/work/matse/blockui/BlockUIService.kt
+++ my_project/block-ui/app/src/main/java/work/matse/blockui/BlockUIService.kt
@@ -1,26 +1,32 @@
 package work.matse.blockui
 
-import android.app.ActivityManager
 import android.app.Service
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import android.graphics.Color
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.PixelFormat
+import android.net.Uri
 import android.os.Build
+import android.os.Environment
 import android.os.IBinder
-import android.util.Log
+import android.provider.MediaStore
+import android.text.format.DateFormat
 import android.view.*
 import android.widget.Button
-import android.widget.TableLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.doOnAttach
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.util.*
+
+
 
 class BlockUIService : Service() {
-    private var layoutOverlay: ConstraintLayout? = null
-    //private var layoutKeyboard: TableLayout? = null
-    private var textViewKey: TextView? = null
     private var windowManager: WindowManager? = null
     private var viewOverlay: View? = null
     private var sharedPreferences: SharedPreferences? = null
@@ -38,19 +44,17 @@ class BlockUIService : Service() {
     }
 
     private fun toggleVisibility() {
-        if (textViewKey!!.visibility == View.VISIBLE) {
-            textViewKey!!.visibility = View.INVISIBLE
+        if (viewOverlay!!.visibility == View.VISIBLE) {
+            viewOverlay!!.visibility = View.INVISIBLE
         }
         else {
-            textViewKey!!.visibility = View.VISIBLE
+            viewOverlay!!.visibility = View.VISIBLE
         }
     }
 
     private fun createView(): View {
         sharedPreferences = baseContext.getSharedPreferences("BlockUI", Context.MODE_PRIVATE)
-        val inflater =
-                baseContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-
+        val inflater = baseContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         viewOverlay = inflater.inflate(R.layout.overlay, null)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -61,8 +65,6 @@ class BlockUIService : Service() {
                 }
             }
         }
-        //вывод оповещения!
-        layoutOverlay = viewOverlay!!.findViewById(R.id.lyOverlay)
 
         viewOverlay!!.findViewById<Button>(R.id.btnBack).setOnClickListener {
             toggleVisibility()
@@ -72,12 +74,8 @@ class BlockUIService : Service() {
     }
 
     private fun generateLayoutParams(): WindowManager.LayoutParams {
-        val layoutParams = WindowManager.LayoutParams(
-                            WindowManager.LayoutParams.MATCH_PARENT,
-                            WindowManager.LayoutParams.MATCH_PARENT,
-                            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                            WindowManager.LayoutParams.FLAG_FULLSCREEN and WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
-                            PixelFormat.TRANSLUCENT)
+        val layoutParams = WindowManager.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN and WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN, PixelFormat.TRANSLUCENT)
 
         layoutParams.gravity = Gravity.TOP or Gravity.RIGHT
         layoutParams.x = 0
@@ -85,5 +83,27 @@ class BlockUIService : Service() {
         return layoutParams
     }
 
+    private fun getScreenShotFromView(v: View): Bitmap? {
+        var screenshot: Bitmap? = null
+        screenshot = Bitmap.createBitmap(v.measuredWidth, v.measuredHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(screenshot)
+        v.draw(canvas)
+        return screenshot
+    }
 
+    private fun saveMediaToStorage(bitmap: Bitmap) {
+        val filename = "${System.currentTimeMillis()}.jpg"
+        var fos: OutputStream? = null
+
+        this.contentResolver?.also { resolver ->
+            val contentValues = ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+            }
+            //вставка в галерею
+            val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            fos = imageUri?.let { resolver.openOutputStream(it) }
+        }
+    }
 }
--- my_project/block-ui/app/src/main/java/work/matse/blockui/MainActivity.kt
+++ my_project/block-ui/app/src/main/java/work/matse/blockui/MainActivity.kt
@@ -7,23 +7,14 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.res.ColorStateList
-import android.graphics.Bitmap
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.os.Handler
 import android.provider.Settings
-import android.text.format.DateFormat
-import android.view.View
 import android.widget.Button
-import android.widget.SeekBar
-import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import java.io.File
-import java.io.FileOutputStream
-import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -41,23 +32,26 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        Handler().postDelayed(Runnable {
-            initService()
-        }, 200)
+        Handler().postDelayed(Runnable { initService() }, 200)
     }
 
     private fun initService() {
         if (checkOverlayDisplayPermission()) {
-            //startService()
+
         }
         else {
             requestOverlayDisplayPermission()
         }
     }
 
-    private fun startService() {
+    private fun startmyService() {
         val overlayService = Intent(this, BlockUIService::class.java)
         ContextCompat.startForegroundService(this, overlayService)
+    }
+
+    private fun stopmyService() {
+        val overlayService = Intent(this, BlockUIService::class.java)
+        stopService(overlayService)
     }
 
     private fun checkOverlayDisplayPermission(): Boolean {
@@ -82,41 +76,13 @@ class MainActivity : AppCompatActivity() {
         if (findViewById<Button>(R.id.btnOn).text == "Включить") {
             findViewById<Button>(R.id.btnOn).setText("Выключить")
             findViewById<Button>(R.id.btnOn).setBackgroundTintList(ColorStateList.valueOf(Color.BLACK))
+            startmyService()
         }
         else {
             findViewById<Button>(R.id.btnOn).setText("Включить")
             findViewById<Button>(R.id.btnOn).setBackgroundTintList(ColorStateList.valueOf(Color.BLUE))
+            stopmyService()
         }
     }
 
-    private fun takeScreenshot() {
-        val now = Date()
-        DateFormat.format("yyyy-MM-dd_hh:mm:ss", now)
-        try {
-            val mPath: String = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg"
-
-            val v1: View = window.decorView.rootView
-            v1.setDrawingCacheEnabled(true)
-            val bitmap: Bitmap = Bitmap.createBitmap(v1.getDrawingCache())
-            v1.setDrawingCacheEnabled(false)
-            val imageFile = File(mPath)
-            val outputStream = FileOutputStream(imageFile)
-            val quality = 100
-            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream)
-            outputStream.flush()
-            outputStream.close()
-            //в этом варианте открыть
-            openScreenshot(imageFile)
-        } catch (e: Throwable) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun openScreenshot(imageFile: File) {
-        val intent = Intent()
-        intent.action = Intent.ACTION_VIEW
-        val uri = Uri.fromFile(imageFile)
-        intent.setDataAndType(uri, "image/*")
-        startActivity(intent)
-    }
 }
